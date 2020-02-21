97652a8ac7e853a3b7e4478643d1fd50385a9b77
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-02-21 10:50:28 +0100

    Updates

---
+++ precache-manifest.ae99a4fbfd669d83f5d3a72511bd3911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3bafa24675730961380ddce9607b8f",
+    "url": "/to-do-redux/index.html"
+  },
+  {
+    "revision": "8956e907086e969b574d",
+    "url": "/to-do-redux/static/css/main.c93df0cb.chunk.css"
+  },
+  {
+    "revision": "c43f557d647938327fed",
+    "url": "/to-do-redux/static/js/2.2c73ef89.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/to-do-redux/static/js/2.2c73ef89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8956e907086e969b574d",
+    "url": "/to-do-redux/static/js/main.104467ee.chunk.js"
+  },
+  {
+    "revision": "471ac5ca6e0b464020c0",
+    "url": "/to-do-redux/static/js/runtime-main.67ef50d6.js"
+  }
+]);
